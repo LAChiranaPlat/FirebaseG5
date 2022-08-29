d3af08f2c7993ddc56f229de5ef423a612289cc0
huarseral <huarseral@hotmail.com>
2022-08-29 17:26:12 -0500

    Vinculación de Vistas

--- app/src/main/java/com/example/firebaseg5/MainActivity.kt
+++ app/src/main/java/com/example/firebaseg5/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.firebaseg5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firebaseg5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var layout:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        layout= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(layout.root)
+
     }
 }
